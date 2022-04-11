8e932d74e056b9bdd903ae9b59d03f102480f52e
sa8tyajeet46 <satyajeettripathy677@gmail.com>
2022-04-11 09:21:42 +0530

    search api created

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const { default: mongoose } = require('mongoose');
 const cors=require('cors');
 const Products=require("./Productmodel");
+const apiFeaturekey = require('./ApiFeatureKey');
 const app=express();
 const port=process.env.PORT || 8000;
 app.use(express.json());
@@ -17,20 +18,22 @@ app.post('/api/products/new',(req,res)=>{
        res.status(201).send(data);
     })
 })
-app.get('/api/products',(req,res)=>{
-    Products.find((err,data)=>{
-       if(err)
-       res.status(500).send(err);
-       else
-       res.status(200).send(data);
-    })
-})
+app.get('/api/products',async (req,res)=>{
+   const product=await new apiFeaturekey(Products.find(),req.query).search().query;
+        if(product)
+        res.status(200).send({"sucess":"true","product":product});
+        else
+        {
+         res.status(500).send({"sucess":"false"});
+        }
+}
+       )
 app.get('/api/product/:id',(req,res)=>{
    Products.find({"_id":req.params.id},(err,data)=>{
     if(err)
-    res.status(500).send("product not found");
+    res.status(500).send({"sucess":"false"});
     else
-    res.status(200).send(data);
+    res.status(200).send({"sucess":"true","product":data});
    })
 })
 app.get('/api/delete/:id',(req,res)=>{
@@ -44,7 +47,7 @@ app.get('/api/delete/:id',(req,res)=>{
  app.put('/api/update/:id',(req,res)=>{
     Products.updateOne({"_id":req.params.id},{$set:req.body},(err,data)=>{
      if(err)
-     res.status(500).send("product not found");
+     res.status(500).send({"sucess":"false"});
      else
      res.status(200).send(data);
     })

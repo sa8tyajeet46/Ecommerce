c86ac74242198c84c816ea8de31bd381e531752f
sa8tyajeet46 <satyajeettripathy677@gmail.com>
2022-04-13 10:28:17 +0530

    Login authentication added

---
+++ backend/Usermodel.js
@@ -0,0 +1,38 @@
+const bcrypt = require("bcryptjs");
+const { default: mongoose } = require("mongoose");
+//const { stringify } = require("nodemon/lib/utils");
+const validator=require("validator");
+const authSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        maxlength:[10,"character shouldnt exceed 10"],
+        minlength:[4,"character must be more than 4"],
+        required:[true,"name cannot be mty"]        
+    },
+    password:{
+        type:String,
+        select:false,
+        maxlength:[10,"character shouldnt exceed 10"],
+        minlength:[4,"character must be more than 4"],
+        required:true
+    },
+    email:{
+        type:String,
+        unique:[true,"already in use"],
+        validate:[validator.isEmail,"enter a valid email"],
+        required:true
+    },
+    role:{
+        type:String,
+        default:"user"
+    },
+    resetPasswordToken:String,
+    resetPasswordExpire:Date
+})
+authSchema.pre('save',async function(next){
+    if(!this.isModified("password")){
+        next();
+    }
+    this.password=await bcrypt.hash(this.password,10);
+})
+module.exports=mongoose.model("auth",authSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,50 @@
 const express=require('express');
 const { default: mongoose } = require('mongoose');
 const cors=require('cors');
+const auth=require('./Usermodel');
 const Products=require("./Productmodel");
 const apiFeaturekey = require('./ApiFeatureKey');
+const bcrypt = require('bcryptjs/dist/bcrypt');
+const jwt = require('jsonwebtoken');
 const app=express();
+const cookieParser=require('cookie-parser');
 const port=process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors())
+app.use(cookieParser());
 const url="mongodb://localhost:27017/Ecommerce";
 mongoose.connect(url).then(()=>console.log("db connected")).catch((err)=>console.log(err));
+const isauthenticateduser=async (req,res,next)=>{
+   const {token}=req.cookies;
+   console.log(token);
+}
+const getJWTToken=async(data)=>{
+   const token=await jwt.sign({id:data._id},"secret",{ expiresIn: '5d'});
+   return token;
+
+}
+const sendToken=async (user,satuscode,res)=>{
+   const token=await getJWTToken(user);
+   const options={
+      expires:new Date(Date.now()+process.env.cookieexpire*24*60*60*1000),
+      httpOnly:true,
+   };
+   res.status(satuscode).cookie("token",token,options).json({
+      sucess:true,
+      user,
+      token,
+   })
+}
+const isauthenticated=async(req,res,next)=>{
+   const {token}=req.cookies;
+  // console.log(token);
+   if(!token)
+   return res.end();
+  //console.log(token);
+   const decodedData=jwt.verify(token,"secret");
+   req.user =await auth.findById(decodedData.id);
+   next();
+}
 app.post('/api/products/new',(req,res)=>{
     const product=req.body;
     Products.create(product,(err,data)=>{
@@ -18,16 +54,36 @@ app.post('/api/products/new',(req,res)=>{
        res.status(201).send(data);
     })
 })
-app.get('/api/products',async (req,res)=>{
+app.get('/api/products',isauthenticated,async (req,res)=>{
    const product=await new apiFeaturekey(Products.find(),req.query).search().query;
         if(product)
-        res.status(200).send({"sucess":"true","product":product});
+  {      res.status(200).send({"sucess":"true","product":product});
+       // console.log(req.cookies);
+}
         else
         {
          res.status(500).send({"sucess":"false"});
         }
 }
        )
+       app.get('/logout',async (req,res)=>{
+       res.cookie("token",null,{
+          expires:new Date(Date.now()),
+          httpOnly:true
+       })
+       res.status(200).send({sucess:true,message:"logged out"});
+       })
+       app.post('/register',async(req,res)=>{
+          const {name,email,password}=req.body;
+          auth.create({name,email,password},(err,data)=>{    
+               if(err)
+          res.status(500).send(err);
+          else
+          {
+            sendToken(data,201,res);
+          }
+          })
+       })
 app.get('/api/product/:id',(req,res)=>{
    Products.find({"_id":req.params.id},(err,data)=>{
     if(err)
@@ -35,6 +91,29 @@ app.get('/api/product/:id',(req,res)=>{
     else
     res.status(200).send({"sucess":"true","product":data});
    })
+})
+app.post('/login',async(req,res)=>{
+   const {email,password}=req.body;
+   if(!email|| !password){
+res.status(500).send({"sucess":"false","msg":"enter email and password"});
+   }
+else{
+ const user=await auth.findOne({email}).select("+password");
+ if(!user){
+    res.status(500).send({"sucess":"false","msg":"incorrect email and password"});
+ }
+ else
+ {
+    const isPasswordMatched=await bcrypt.compare(password,user.password);
+    if(isPasswordMatched)
+    {
+     sendToken(user,200,res);
+    }
+ else
+ res.status(401).send({"sucess":"false","msg":"invalid credentials"})
+ }
+}
+   
 })
 app.get('/api/delete/:id',(req,res)=>{
     Products.deleteOne({"_id":req.params.id},(err,data)=>{

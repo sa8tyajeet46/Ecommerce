3b3cc17615b674edd8f445c6055abf82fbbfcad8
sa8tyajeet46 <satyajeettripathy677@gmail.com>
2022-05-02 08:50:11 +0530

    completed

---
+++ README.md
@@ -0,0 +1,23 @@
+## MERN Stack Ecommerce Website
+
+Link to the [Ecommerce website](https://ecommerce-d2ae5.web.app)
+
+### Tech Stack used
+  
+* Frontend
+   * React
+   * Redux for state management
+   * Tailwind css
+   * Axios for database connection
+* Backend
+   * Mongodb
+   * Express
+   * JWT token for user signin
+
+
+### Features
+
+
+* User Authentication
+* Hashed Password by Bycrypt.js
+* Fully responsive Ecommerce model using MERN
--- frontend/src/action/getallproduct.js
+++ frontend/src/action/getallproduct.js
@@ -1,11 +1,11 @@
 import {ALL_PRODUCT_FAIL,ALL_PRODUCT_REQUEST,ALL_PRODUCT_SUCESS} from '../constants/productconstants';
-import axios from 'axios';
+import axios from '../axios';
 export const getProducts=(keyword)=>async (dispatch)=>{
     try{
         dispatch({
             type:ALL_PRODUCT_REQUEST
         })
-        const {data}=await axios.get(`/api/products?keyword=${keyword}`);
+        const {data}=await axios.get(`api/products?keyword=${keyword}`);
        dispatch({
            type:ALL_PRODUCT_SUCESS,
            payload:data
